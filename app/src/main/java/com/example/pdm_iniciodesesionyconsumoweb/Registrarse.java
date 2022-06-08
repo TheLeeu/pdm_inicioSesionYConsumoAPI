745d49539064bbea30beeec662e1d25413c61ac7
TheLeeu <arandaantonio148@gmail.com>
2022-06-08 16:04:38 -0600

    se conecto la interfaz con el codigo en la actividad Registrarse

--- app/src/main/java/com/example/pdm_iniciodesesionyconsumoweb/Registrarse.java
+++ app/src/main/java/com/example/pdm_iniciodesesionyconsumoweb/Registrarse.java
@@ -3,12 +3,17 @@ package com.example.pdm_iniciodesesionyconsumoweb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Registrarse extends AppCompatActivity {
+    private EditText et_email, et_c1,et_c2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+        et_email = (EditText) findViewById(R.id.etEmailR);
+        et_c1 = (EditText) findViewById(R.id.etContrasenia1);
+        et_c2 = (EditText) findViewById(R.id.etContrasenia2);
     }
 }
