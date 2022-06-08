f4fa56841a00c8ecc0ca7bbed3e036b87a5f897a
TheLeeu <arandaantonio148@gmail.com>
2022-06-08 16:42:05 -0600

    Se agrego el inicio de sesion con usuario registrado

--- app/src/main/java/com/example/pdm_iniciodesesionyconsumoweb/MainActivity.java
+++ app/src/main/java/com/example/pdm_iniciodesesionyconsumoweb/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pdm_iniciodesesionyconsumoweb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         et_email = (EditText)  findViewById(R.id.etEmail);
         et_contrasenia = (EditText) findViewById(R.id.etContrasenia);
+        
     }
 
     public void btnRegistrarse(View view) {
@@ -39,6 +44,29 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
             Toast.makeText(this, "Sesion esta iniciada", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void btnLogin(View view) {
+        //Valida que campos no esten vacios
+        if(!et_email.getText().toString().isEmpty() && !et_contrasenia.getText().toString().isEmpty()){
+            //Acceder al usuario
+            mAuth.signInWithEmailAndPassword(et_email.getText().toString(), et_contrasenia.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Iniciando sesion", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(MainActivity.this, "Fallo al iniciar sesion", Toast.LENGTH_SHORT).show();
+                                et_email.setText("");
+                                et_contrasenia.setText("");
+
+                            }
+                        }
+                    });
+        }else{
+            Toast.makeText(this, "Debe llenar todos los campos", Toast.LENGTH_SHORT).show();
         }
     }
 }
