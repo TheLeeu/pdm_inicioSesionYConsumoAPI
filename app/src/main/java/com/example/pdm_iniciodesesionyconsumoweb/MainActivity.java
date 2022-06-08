7991d7297f2f733af5fcea24e2c25301fc60074b
TheLeeu <arandaantonio148@gmail.com>
2022-06-08 16:27:28 -0600

    Cuando inicialices la actividad, verifica que el usuario haya accedido

--- app/src/main/java/com/example/pdm_iniciodesesionyconsumoweb/MainActivity.java
+++ app/src/main/java/com/example/pdm_iniciodesesionyconsumoweb/MainActivity.java
@@ -5,17 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void btnRegistrarse(View view) {
         Intent intent = new Intent(this, Registrarse.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(this, "Sesion esta iniciada", Toast.LENGTH_SHORT).show();
+        }
     }
 }
