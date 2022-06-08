1ddcacf1d9b49035f687e9a95834422bb90e94af
TheLeeu <arandaantonio148@gmail.com>
2022-06-08 16:08:10 -0600

    se agrego e inicializo una variable de tipo FirebaseAuth para usar firebase y se agrego el evento click del boton de registro

--- app/src/main/java/com/example/pdm_iniciodesesionyconsumoweb/Registrarse.java
+++ app/src/main/java/com/example/pdm_iniciodesesionyconsumoweb/Registrarse.java
@@ -3,10 +3,14 @@ package com.example.pdm_iniciodesesionyconsumoweb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registrarse extends AppCompatActivity {
     private EditText et_email, et_c1,et_c2;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,12 @@ public class Registrarse extends AppCompatActivity {
         et_email = (EditText) findViewById(R.id.etEmailR);
         et_c1 = (EditText) findViewById(R.id.etContrasenia1);
         et_c2 = (EditText) findViewById(R.id.etContrasenia2);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void btnRegistro(View view) {
+        
     }
 }
